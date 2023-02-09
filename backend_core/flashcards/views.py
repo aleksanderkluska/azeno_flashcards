c0b7933273bb2af031b44ac9a698e0bbcb2d464b
Aleksander Kluska <kluskaolek2002@gmail.com>
2023-02-09 17:01:19 +0100

    basic views and serializers for excercises

--- backend_core/flashcards/views.py
+++ backend_core/flashcards/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from .models import FlashCard
+from .serializers import FlashCardSerializer
+
+
+class FlashCardView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = FlashCardSerializer
+
+    def get_queryset(self):
+        rating = self.request.query_params.get('rating', 0)
+
+        return FlashCard.objects.filter(rating__gte=rating)
+
+
+class FlashCardItemView(RetrieveUpdateDestroyAPIView):
+    queryset = FlashCard.objects.all()
+    serializer_class = FlashCardSerializer
